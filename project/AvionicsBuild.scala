6395b861136bf7f23805ed4734b50948e576328e
Marcos Matos <marcosccm@gmail.com>
2013-08-21 09:57:09 -0300

    Add test dependencies

--- project/AvionicsBuild.scala
+++ project/AvionicsBuild.scala
@@ -13,7 +13,11 @@ object AvionicsBuild extends Build {
       scalaVersion := "2.10.0",
       scalacOptions ++= Seq("-feature", "-deprecation"),
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.2"
+      libraryDependencies ++= Seq(
+        "org.scalatest" %% "scalatest" % "2.0.M5b" % "test", 
+        "com.typesafe.akka" %% "akka-testkit" % "2.1.0",
+        "com.typesafe.akka" %% "akka-actor" % "2.1.2"
+      )
     )
   )
 }
